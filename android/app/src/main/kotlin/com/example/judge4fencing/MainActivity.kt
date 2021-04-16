f16e5c79824ba8509f77781936eccd51e965a696
demilei221 <61996987+demilei221@users.noreply.github.com>
2021-04-15 21:07:59 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/judge4fencing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.judge4fencing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/LearnScreen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class LearnPage extends StatefulWidget {
+  LearnPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _LearnPageState createState() => _LearnPageState();
+}
+
+class _LearnPageState extends State<LearnPage> {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  // final List<int> colorCodes = <int>[600, 500, 100];
+
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            color: Colors.amber[300],
+            child: Center(child: Text('Entry ${entries[index]}')),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      )// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
